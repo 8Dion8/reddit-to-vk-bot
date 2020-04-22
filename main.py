c45a0d1412a89797821157eb5cd057cf8af65cb9
8Dion8 <62215043+8Dion8@users.noreply.github.com>
2020-04-22 21:07:47 +0300

    Added the main program

---
+++ main.py
@@ -0,0 +1,11 @@
+cache_thoughts = []
+SUBREDDIT = 'showerthoughts'
+Bot = InstaBot()
+while True:
+    op_user, thought, upvotes, link = Bot.get_from_reddit(SUBREDDIT)
+    if thought not in cache_thoughts:
+        cache_thoughts.append(thought)
+        translated = Bot.translate(thought)
+        post = 'Post on r/' + SUBREDDIT + ' by ' + op_user + ' (' + upvotes + ' upvotes)' + '\n\n\n' + thought + '\n\n\n' + 'Перевод на русский от Google Translate:\n' + translated + '\n\n\nLink: ' + link + "\n\n\nЯ бот! Жалобы/предложения писать в лс"
+        Bot.post_to_vk('login','password',post)
+    sleep(60)
